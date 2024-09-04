291d638471764bed6232749354b75f4a7b10eeb0
JadeGravelle <ri376260@ucf.edu>
2024-09-04 03:23:35 +0000

    Finished

--- fracturing.c
+++ fracturing.c
@@ -16,15 +16,15 @@ double askForUserInput(const char* point, const char* coordinate) {
 // Function to calculate distance between two points
 double calculateDistance() {
     double x1 = askForUserInput("Point #1", "x1");
-    double y1 = askForUserInput("Point #1", "y1");
     double x2 = askForUserInput("Point #2", "x2");
+    double y1 = askForUserInput("Point #1", "y1");
     double y2 = askForUserInput("Point #2", "y2");
 
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 
-    printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
-    printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
-    printf("The distance between the two points is %.2f\n", distance);
+    printf("Point #1 entered: x1 = %.3f; y1 = %.3f\n", x1, y1);
+    printf("Point #2 entered: x2 = %.3f; y2 = %.3f\n", x2, y2);
+    printf("The distance between the two points is %.3f\n", distance);
 
     return distance;
 }
@@ -36,7 +36,7 @@ double calculatePerimeter() {
     // Assuming the perimeter is 4 times the distance 
     double perimeter = 4 * distance;
 
-    printf("The perimeter of the city encompassed by your request is %.2f\n", perimeter);
+    printf("The perimeter of the city encompassed by your request is %.3f\n", perimeter);
     return 2.0;  
 }
 
@@ -47,7 +47,7 @@ double calculateArea() {
     // Assuming the area is the square of the distance 
     double area = pow(distance, 2);
 
-    printf("The area of the city encompassed by your request is %.2f\n", area);
+    printf("The area of the city encompassed by your request is %.3f\n", area);
     return 1.0;  
 }
 
@@ -58,9 +58,9 @@ double calculateWidth() {
 
     double width = fabs(x2 - x1);
 
-    printf("Point #1 entered: x1 = %.2f; y1 = (not needed)\n", x1);
-    printf("Point #2 entered: x2 = %.2f; y2 = (not needed)\n", x2);
-    printf("The width of the city encompassed by your request is %.2f\n", width);
+    printf("Point #1 entered: x1 = %.3f; y1 = (not needed)\n", x1);
+    printf("Point #2 entered: x2 = %.3f; y2 = (not needed)\n", x2);
+    printf("The width of the city encompassed by your request is %.3f\n", width);
 
     return 2.0;  
 }
@@ -72,9 +72,9 @@ double calculateHeight() {
 
     double height = y2 - y1;
 
-    printf("Point #1 entered: x1 = (not needed); y1 = %.2f\n", y1);
-    printf("Point #2 entered: x2 = (not needed); y2 = %.2f\n", y2);
-    printf("The height of the city encompassed by your request is %.2f\n", height);
+    printf("Point #1 entered: x1 = (not needed); y1 = %.3f\n", y1);
+    printf("Point #2 entered: x2 = (not needed); y2 = %.3f\n", y2);
+    printf("The height of the city encompassed by your request is %.3f\n", height);
 
     return 2.0;  
 }
